b5842761eb706d7ee3bbce3ee26d2da7e915a933
kimkimsh <kimkimsh@users.noreply.github.com>
2020-05-25 18:27:40 +0900

    Update UV_sterilizer.ino

--- UV_sterilizer/UV_sterilizer.ino
+++ UV_sterilizer/UV_sterilizer.ino
@@ -93,13 +93,13 @@ void sound()
     // to calculate the note duration, take one second divided by the note type.
     //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
     int noteDuration = 1000 / noteDurations[thisNote];
-    tone(4, melody[thisNote], noteDuration);
+    tone(buzzer, melody[thisNote], noteDuration);
 
     // to distinguish the notes, set a minimum time between them.
     // the note's duration + 30% seems to work well:
     int pauseBetweenNotes = noteDuration * 1.30;
     delay(pauseBetweenNotes);
     // stop the tone playing:
-    noTone(4);
+    noTone(buzzer);
   }
 }
